b3bf2a2877f2763162f7d65126988dad5509a2eb
Matthew DeJonge <mdejonge@terryberry.com>
2026-08-13 16:29:06 -0400

    default: more messages

--- src/app/components/results-dialog/results-dialog-data.ts
+++ src/app/components/results-dialog/results-dialog-data.ts
@@ -6,19 +6,32 @@ const BANKING_GENERAL = [
 
 const BANKING_LOW = [
   ...BANKING_GENERAL,
-  'Lame'
+  'Lame',
+  'If I broke every bone in your hand, could you still do that?',
+  'Did you get a group rate or something?',
+  `You've gotta walk before you crawl.`,
+  `Why are you such a wet sandwich?`,
+  `I'm just going to put dirt in my ears.`
 ] as const;
 
 const BANKING_HIGH = [
   ...BANKING_GENERAL,
   'Good roll, good roll.',
-  `NOW THAT'S A LOT OF DAMAGE!`
+  `NOW THAT'S A LOT OF DAMAGE!`,
+  'Do you know what Chuck Berry said every night before counting 1, 2, 3, 4?',
+  'Anybody want a peanut?',
+  'A titanium card! How the cuss are you qualified for that?',
+  `The house safe is for brandy and grandmother's pearls.`
 ] as const;
 
 const FARKLE = [
   `You'll get nothing and like it!`,
   `Respect the 2's`,
-  'Nice one dips...'
+  'Nice one dips...',
+  'You fell victim to one of the classic blunders!',
+  `You're in a puddle of shit and you don't have the shoes for it.`,
+  'Inconceivable!',
+  `You've got one foot in the grave and three feet on a banana peel.`
 ] as const;
 
 export interface ResultsDialogData {
